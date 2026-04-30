0321c548739e29aeb919b6cee16b88b5accafa4c
KehtuKumar <fortesting626622.com>
2026-04-30 23:23:32 +0530

    fix axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: import.meta.env.VITE_API_BASE_URL,
-  baseURL:"https://recipe-box-84ms.onrender.com/api",
+  baseURL:"https://recipe-box-84ms.onrender.com",
   withCredentials: true
 });
 
