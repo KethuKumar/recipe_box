4b457ce5f45574487083afc2ebffd775316b1fb8
KehtuKumar <fortesting626622.com>
2026-04-30 23:50:58 +0530

    update authslice

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -31,9 +32,25 @@ const userSchema = new mongoose.Schema(
       },
     ],
   },
-  { timestamps: true },
+  { timestamps: true }
 );
 
-const userModel = mongoose.model("User",userSchema)
 
-export default userModel
+// 🔥 HASH PASSWORD BEFORE SAVE
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+
+// 🔥 COMPARE PASSWORD METHOD
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;
