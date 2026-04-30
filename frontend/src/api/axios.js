fcc500a7ae0fa394d6d231af55911adae1dcf065
KehtuKumar <fortesting626622.com>
2026-04-30 17:07:52 +0530

    kk1

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  // baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL:"https://recipe-box-84ms.onrender.com",
   withCredentials: true
 });
 
